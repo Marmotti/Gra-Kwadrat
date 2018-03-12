aeb501dc15237825c57305ded6fd1cf17481ea25
unknown <Marmotti>
2018-03-12 18:19:15 +0100

    Added HUD and basic enemy. Implemented the borders of screen.

--- src/com/tutorial/main/HUD.java
+++ src/com/tutorial/main/HUD.java
@@ -4,12 +4,16 @@ import java.awt.*;
 
 public class HUD {
 
-    public static int HEALTH = 100;
+    public static int HEALTH = 80;
     public void tick(){
 
     }
     public void render(Graphics g){
         g.setColor(Color.gray);
         g.fillRect(Game.WIDTH/2 - 50, Game.HEIGHT - Game.HEIGHT/5, 100, 20);
+        g.setColor(Color.lightGray);
+        g.fillRect(Game.WIDTH/2 - 50, Game.HEIGHT - Game.HEIGHT/5, 100 - HEALTH, 20);
+        g.setColor(Color.white);
+        g.drawRect(Game.WIDTH/2 - 50, Game.HEIGHT - Game.HEIGHT/5, 100, 20);
     }
 }
